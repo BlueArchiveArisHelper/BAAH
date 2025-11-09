61d40ac1f8f02c1b5d30e655f12f5a004ff1a727
yusen <sanmusen_wu@outlook.com>
2025-11-09 15:37:44 +0800

    1.11.2

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -1,8 +1,30 @@
-myversion = "1.11.1"
+myversion = "1.11.2"
 mychangelog = """
 
-## BAAH new workflow
+## Fix
 
-Auto package, tag and release!
+修复国服反和谐后会卡在登录社区弹窗三十秒的问题
+
+修复竞技场助战选择学生失败的问题
+
+Fix Contest can not select help student
+
+## Feat
+
+（由于是实验性功能，有问题请到q群反馈985729020）
+
+新增GUI操作流实现 更易上手，更高自由度，更稳定 的开启加速器和自定义任务流程
+
+More flexible user define tasks
+
+## Update
+
+以前的开启加速器流程迁移至新的操作流。按理说自动迁移应该没什么问题，建议各位sensei更新后观察下开启和关闭加速器流程是否正常
+
+## Build
+
+使用github自动构建并发布release
+
+Auto-release in github action
 
 """
