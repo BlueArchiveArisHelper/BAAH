fb8e9812bb54718beaf230290e379f236f79e145
yusen <sanmusen_wu@outlook.com>
2025-11-30 17:55:14 +0800

    1.11.6

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -1,4 +1,4 @@
-myversion = "1.11.5"
+myversion = "1.11.6"
 mychangelog = """
 
 > BAAH_GUI.exe 已合并至 BAAH.exe, 请点击 BAAH.exe 打开程序 <
@@ -7,24 +7,8 @@ mychangelog = """
 
 ## Fix
 
-修复 H9-2, H14-1 推图
+修复日服一件扫荡点击保存问题
 
-Fix H9-2, H14-1 Quest Explore
-
-修复 更新器 完成更新检查后会直接关闭不会停留的问题
-
-Fix updater closing immediately after checking updates
-
-## Feat
-
-（由于是实验性功能，有问题请到q群反馈985729020）
-
-操作流新增滑动操作
-
-Add scroll action in operation flow
-
-在账号登录页面可以执行自定义操作流
-
-Can do custom actions flow in account login page
+Fix JP server one-click sweep issue
 
 """
