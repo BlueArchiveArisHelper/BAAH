83c2158f4f7d8abbc34eb83d346ec7dfd4ad2078
yusen.wu <sanmusen_wu@outlook.com>
2025-12-17 18:00:33 +0800

    2.0.3

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -1,17 +1,17 @@
-myversion = "2.0.0"
+myversion = "2.0.3"
 mychangelog = """
 
->> 2.0.0 版本需要重新下载完整包，无法由1.x版本升级 | Must download full BAAH 2.0.0 <<
+>> 2.0.0 版本需要重新下载，无法由1.x版本升级 | Must download full BAAH 2.0.0 <<
 
 >> 下载Download: https://baah.sanmusen.top/zh_CN/download.html <<
 
 >> 迁移配置文件: 将2.0.0版本解压至旧版本目录覆盖 或 手动迁移旧版本的 BAAH_CONFIGS 和 DATA/CONFIGS 文件夹 <<
 
->> Retract zip into old version folder or Move files in BAAH_CONFIGS and DATA/CONFIGS folder to keep configs <<
+>> Keep configs: Retract zip into old version folder or Move files in BAAH_CONFIGS and DATA/CONFIGS folder <<
 
 # 更新
 
-支持 Steam 的碧蓝档案
+支持 Steam 上的碧蓝档案
 
 Support Steam Blue Archive
 
