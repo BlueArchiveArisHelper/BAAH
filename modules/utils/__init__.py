ad46384cb3db197ab9da2caa9d1bcbefccdec87a
yusen <sanmusen_wu@outlook.com>
2025-12-25 23:24:20 +0800

    chore: text

--- modules/utils/__init__.py
+++ modules/utils/__init__.py
@@ -280,9 +280,14 @@ def check_connect():
                     return True
             logging.error({"zh_CN": "图片分辨率不为1280*720，请设置模拟器分辨率为1280*720（当前{}*{}）".format(wm_width, wm_height), "en_US":"The resolution is not 1280*720, please set the resolution to 1280*720 (current {}*{})".format(wm_width, wm_height)})
             raise Exception("图片分辨率不为1280*720，请设置模拟器分辨率为1280*720（当前{}*{}）".format(wm_width, wm_height))
+    else:
+        logging.error(istr({
+            CN: f"截图文件为空",
+            EN: "Screenshot is null"
+        }))
     logging.error({"zh_CN": "模拟器adb或ba程序连接失败", "en_US":"Failed to connect to the emulator or ba program"})
     logging.warn({"zh_CN": "模拟器请检查adb与模拟器连接端口号是否正确，Steam端请检查游戏是否在更新", "en_US":"Please check if the adb and emulator connection port number is correct or Steam ba is updating"})
-    if "127.0.0.1" in getNewestSeialNumber():
+    if not _is_steam_app(config.userconfigdict["SERVER_TYPE"]) and "127.0.0.1" in getNewestSeialNumber():
         logging.warn({"zh_CN": "请确保关闭模拟器网络桥接", "en_US":"Please check if the emulator network bridging is turned off"})
     return False
 
