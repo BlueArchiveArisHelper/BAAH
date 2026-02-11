20c62384ed67af216e576dbb2135a0935b594c26
Yusen Wu <sanmusen_wu@outlook.com>
2026-02-11 15:48:12 +0800

    2.1.1

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -1,13 +1,9 @@
-myversion = "2.1.0"
+myversion = "2.1.1"
 mychangelog = """
 >> BAAH2: https://baah.sanmusen.top/zh_CN/download.html <<
 
->> 迁移配置: 请手动迁移 BAAH_CONFIGS 和 DATA/CONFIGS 文件夹 | Move files in BAAH_CONFIGS and DATA/CONFIGS folder to keep configs <<
+修复PC端活动页面错误问题 | Fix Bugs in Event Page for PC
 
-# Update
-
-更新了 小人战斗 的脚本任务逻辑 | Updated the task logic of Little People Fighting part
-
-添加了PC端运行指引文字 | Added instructions for running on PC
+修复国服战斗页面识别失效问题 | Fix known issue
 
 """
