28376ed9fd9d74e4521f349c164f9447ae011a61
yusen.wu <sanmusen_wu@outlook.com>
2026-03-10 18:02:56 +0800

    2.2.3

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -1,9 +1,9 @@
-myversion = "2.2.1"
+myversion = "2.2.3"
 mychangelog = """
 >> BAAH2: https://baah.sanmusen.top/ <<
 
 优化 GUI 界面 | optimize GUI interface
 
-修复了部分已知问题 | fixed some known issues
+修复了2.2.1 GUI看不到任务列表问题 | fixed the problem that the task list is not visible in 2.2.1 GUI
 
 """
