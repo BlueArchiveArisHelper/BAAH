c9bfd52c5be4b55ad3762c3e3780043a4a0b54c1
yusen.wu <sanmusen_wu@outlook.com>
2026-03-13 18:58:33 +0800

    2.2.5

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -2,6 +2,8 @@ myversion = "2.2.5"
 mychangelog = """
 >> BAAH2: https://baah.sanmusen.top/ <<
 
+添加大决战任务 (#340) | add big battle task (#340)
+
 修复 GUI 界面固定为 深色 模式的问题 | fix GUI interface fixed to dark mode issue
 
 修复 大更新识别失效问题 | fix big update recognition failure issue
