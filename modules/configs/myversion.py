b710b60f1b1b9a08c977fb97b645768d5317a0a6
yusen.wu <sanmusen_wu@outlook.com>
2026-03-13 18:54:59 +0800

    2.2.5

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -1,7 +1,9 @@
-myversion = "2.2.4"
+myversion = "2.2.5"
 mychangelog = """
 >> BAAH2: https://baah.sanmusen.top/ <<
 
-修复 STEAM 端登陆点不掉活动弹窗的bug | Fix the bug that the event popup cannot be clicked when logging in on STEAM
+修复 GUI 界面固定为 深色 模式的问题 | fix GUI interface fixed to dark mode issue
+
+修复 大更新识别失效问题 | fix big update recognition failure issue
 
 """
