573abd8957ff1122ac1bf8325882b15d9f9ced06
yusen <sanmusen_wu@outlook.com>
2026-03-23 17:22:44 +0800

    2.2.9

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -1,8 +1,8 @@
-myversion = "2.2.8"
+myversion = "2.2.9"
 mychangelog = """
 >> BAAH2: https://baah.sanmusen.top/ <<
 
 添加大决战任务 (#340) | add big battle task (#340)
 
-修复 国服资源量 ocr 错误 | fix resource ocr error in CN server
+修复 活动临近结束弹窗必须得点击蓝色按钮才能关闭 | fix the pop-up window that must click the blue button to close about the event is about to end
 """
