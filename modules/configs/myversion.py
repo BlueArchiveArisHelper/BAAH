7b25c997f6e29a4bae6916ba865cdba3ed29f18b
yusen.wu <sanmusen_wu@outlook.com>
2026-04-22 18:59:15 +0800

    2.2.12

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -1,10 +1,6 @@
-myversion = "2.2.11"
+myversion = "2.2.12"
 mychangelog = """
 >> BAAH2: https://baah.sanmusen.top/ <<
 
-添加大决战任务 (#340) | add big battle task (#340)
-
-修复 国服活动奖励领取失败 | fix failure to claim rewards for CN server events
-
-完善 linux 支持 | improve linux support
+适配日服新GUI | fit new GUI of JP version
 """
