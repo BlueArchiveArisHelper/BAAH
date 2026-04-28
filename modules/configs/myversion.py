4aaf854aadda1c81fd402ba8f8619d452779359e
yusen <sanmusen_wu@outlook.com>
2026-04-28 14:31:04 +0800

    2.2.13

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -1,6 +1,8 @@
-myversion = "2.2.12"
+myversion = "2.2.13"
 mychangelog = """
 >> BAAH2: https://baah.sanmusen.top/ <<
 
 适配日服新GUI | fit new GUI of JP version
+
+修复部分主页live2D会导致卡死在主页 | fix some homepage live2D will cause freeze on homepage
 """
