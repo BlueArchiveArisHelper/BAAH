6e4be5d1e699cb43e929558a8cd699489cc017e2
yusen <sanmusen_wu@outlook.com>
2026-06-11 19:23:20 +0800

    2.2.17

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -1,6 +1,10 @@
-myversion = "2.2.16"
+myversion = "2.2.17"
 mychangelog = """
 >> BAAH2: https://baah.sanmusen.top/ <<
 
+修复推图可能卡在奖励确认页面的问题 | Fix the problem that the program may get stuck on the reward confirmation page during map pushing
+
 修复竞技场队伍有优香的话竞技场无法正常出击的bug | fix the bug that if you have Yukari in your team, you cannot start contest battle
+
+适配国际服新活动一栏入口 | Adapt to the new event recap entrance in global version
 """
