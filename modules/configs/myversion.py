e948cbac3c5e56492c4ee701ca4fa4488a9d0689
yusen.wu <sanmusen_wu@outlook.com>
2026-06-30 15:04:18 +0800

    2.2.19

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -1,7 +1,8 @@
-myversion = "2.2.18"
+myversion = "2.2.19"
 mychangelog = """
->> BAAH2: https://baah.sanmusen.top/ <<
 
-加强课程表优先点击学生的匹配稳定性 | Enhanced the stability of clicking on the student's avatar first in the schedule
+适配日服第二部主线剧情 | Feat The main-second Story (by Tangbelief)
+
+适配 mumu 15.0 启动器路径填写 | Feat mumu 15.0 Launcher Path Fill
 
 """
