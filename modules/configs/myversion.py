7dbf06246f3d90de845fab2a4ceddbf347ba4b61
yusen <sanmusen_wu@outlook.com>
2026-07-11 08:56:41 +0800

    2.2.22

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -3,5 +3,7 @@ mychangelog = """
 
 小工具页面新增推活动剧情和关卡任务 | Addon at the small tool page for pushing event stories and quest tasks
 
+修复日服点进活动坐标靠下导致点进小活动导致脚本报错问题 | Fixed the issue where clicking into the event on the Japanese server would cause the script to report an error due to the coordinates being too low
+
 
 """
