4c1d1b44342e73a6b1dbe4cfd8dbe0575b0fb3c4
yusen <sanmusen_wu@outlook.com>
2026-08-18 23:10:44 +0800

    2.4.6

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -1,6 +1,12 @@
-myversion = "2.4.5"
+myversion = "2.4.6"
 mychangelog = """
 
+更新后如国际服仍有问题请进群反馈 985729020
+
+---
+
 适配国际服更新 | Fit Global Server Update
+
+每日流程页面样式优化 | Daily Task Page CSS Optimization
 
 """
