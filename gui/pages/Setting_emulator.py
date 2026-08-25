5f69ea93c83bcb1f7bf00e0893fccc0bec007202
BlockHaity <1216692740@qq.com>
2026-08-25 15:21:51 +0000

    Feat: 添加可选取消在check_connect中不进行disconnect

--- gui/pages/Setting_emulator.py
+++ gui/pages/Setting_emulator.py
@@ -35,6 +35,8 @@ def set_emulator(config):
     #  大更新配置
         ui.checkbox(config.get_text("config_big_update")).bind_value(config.userconfigdict, "BIG_UPDATE")
 
+    ui.checkbox(config.get_text("adb_disable_auto_disconnect"), value=config.userconfigdict['ADB_DISABLE_AUTO_DISCONNECT']).props('inline').bind_visibility_from(config.userconfigdict, "SERVER_TYPE", lambda v: not _is_PC_app(v))
+    
     with ui.row().bind_visibility_from(config.userconfigdict, "SERVER_TYPE", lambda v: not _is_PC_app(v)):
         ui.label(config.get_text("big_update_type")).style('font-size: medium').bind_visibility_from(config.userconfigdict, "BIG_UPDATE", lambda x: x)
     
