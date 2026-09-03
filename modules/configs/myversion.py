dd5e30f727b3ff32859b505c962cea709c2f40a6
yusen <sanmusen_wu@outlook.com>
2026-09-03 09:53:21 +0800

    2.4.10

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -1,5 +1,7 @@
-myversion = "2.4.9"
+myversion = "2.4.10"
 mychangelog = """
+
+修复国际服旧活动排序在新活动前的问题 | Fix the problem that the old event in the international server is sorted before the new event
 
 修复部分服务器悬赏通缉/交流会/特殊作战扫荡关卡匹配弹窗失败的问题 | Fix the problem of failing to match the popup when raiding levels in some servers' Wanted/Exchange/Special operations
 
